16dd9f02919dc5be218276ff6ef6fa87b349f822
Mattox0 <matbegaaar@gmail.com>
2024-06-05 14:38:50 +0200

    feat(filters): #MED-276 remove advanced search

--- frontend/src/core/enum/advanced-search-sources.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum AdvancedSearchSourcesEnum {
-  GAR = "fr.openent.mediacentre.source.GAR",
-  Moodle = "fr.openent.mediacentre.source.Moodle",
-  Signet = "fr.openent.mediacentre.source.Signet",
-}
--- frontend/src/core/enum/advanced-search.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum AdvancedSearchEnum {
-  title = "title",
-  author = "authors",
-  editor = "editors",
-  discipline = "disciplines",
-  level = "levels",
-  source = "sources",
-}
