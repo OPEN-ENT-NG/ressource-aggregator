3c218802144eb65266d2555816ebe54b53c15d36
Mattox0 <matbegaaar@gmail.com>
2024-06-05 14:48:37 +0200

    feat(filters): #MED-276 remove advanced-search

--- frontend/src/model/AdvancedSearchData.ts
+++
@@ -1,9 +0,0 @@
-import { FieldData } from "./FieldData";
-
-export interface AdvancedSearchData {
-  title?: FieldData;
-  authors?: FieldData;
-  editors?: FieldData;
-  disciplines?: FieldData;
-  levels?: FieldData;
-}
