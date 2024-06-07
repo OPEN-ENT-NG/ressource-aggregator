4bc56cdff8c59c713941d1b059e4f88eaf88a951
Mattox0 <matbegaaar@gmail.com>
2024-06-07 14:54:23 +0200

    fix(tooltip): #MED-278 prettier

--- frontend/src/components/resource/Resource.tsx
+++ frontend/src/components/resource/Resource.tsx
@@ -147,7 +147,9 @@ export const Resource: React.FC<ResourceProps> = ({
               <ContentCopyIcon className="med-link" onClick={() => copy()} />
             </Tooltip>
             {favorite ? (
-              <Tooltip message={t("mediacentre.card.unfavorite")} placement="top"
+              <Tooltip
+                message={t("mediacentre.card.unfavorite")}
+                placement="top"
               >
                 <StarIcon className="med-star" onClick={() => unfav()} />
               </Tooltip>
@@ -202,7 +204,9 @@ export const Resource: React.FC<ResourceProps> = ({
               <ContentCopyIcon className="med-link" onClick={() => copy()} />
             </Tooltip>
             {favorite ? (
-              <Tooltip message={t("mediacentre.card.unfavorite")} placement="top"
+              <Tooltip
+                message={t("mediacentre.card.unfavorite")}
+                placement="top"
               >
                 <StarIcon className="med-star" onClick={() => unfav()} />
               </Tooltip>
