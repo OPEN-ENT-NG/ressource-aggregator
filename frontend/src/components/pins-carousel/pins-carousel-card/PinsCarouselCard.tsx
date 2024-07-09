6aeb48e7391250a4e350cd3348def6f495bc7abb
Nikolaï AMOSSE <nikolai.93@outlook.com>
2024-07-09 10:27:21 +0200

    feat(carousel): #MED-247 create pin cards

--- frontend/src/components/pins-carousel/pins-carousel-card/PinsCarouselCard.tsx
+++ frontend/src/components/pins-carousel/pins-carousel-card/PinsCarouselCard.tsx
@@ -1,14 +1,17 @@
 import { Card } from "@edifice-ui/react";
 import React from "react";
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 
 import { PinResource } from "~/model/PinResource.model";
 import "./PinsCarouselCard.scss";
+import { useTranslation } from "react-i18next";
 
 interface PinsCarouselCardProps {
   pin: PinResource;
 }
 
 export const PinsCarouselCard: React.FC<PinsCarouselCardProps> = ({ pin }) => {
+  const { t } = useTranslation();
   return (
   <Card
     isClickable={false}
@@ -20,6 +23,10 @@ export const PinsCarouselCard: React.FC<PinsCarouselCardProps> = ({ pin }) => {
     <Card.Body space={"0"}>
       <img src={pin.image} alt={pin.pinned_title} className="med-pin-image" />
     </Card.Body>
+    <Card.Footer>
+      <AutoAwesomeIcon />
+      {t("mediacentre.card.offered.by.the.region")}
+    </Card.Footer>
   </Card>
   );
 };
