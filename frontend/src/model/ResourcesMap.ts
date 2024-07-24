5c4e59eeec10294bb871c7a42b34c977642ef142
Nikolaï AMOSSE <nikolai.93@outlook.com>
2024-07-24 15:09:28 +0200

    feat(filter): #MED-319 change types of resourcesMap

--- frontend/src/model/ResourcesMap.ts
+++ frontend/src/model/ResourcesMap.ts
@@ -1,8 +1,10 @@
-import { Resource } from "./Resource.model";
-
+import { Textbook } from "./Textbook.model";
+import { ExternalResource } from "./ExternalResource.model";
+import { Moodle } from "./Moodle.model";
+import { Signet } from "./Signet.model";
 export interface ResourcesMap {
-  textbooks: Resource[];
-  externalResources: Resource[];
-  moodle: Resource[];
-  signets: Resource[];
+  textbooks: Textbook[];
+  externalResources: ExternalResource[];
+  moodle: Moodle[];
+  signets: Signet[];
 }
